7ff9dd0fad2f72a3dcd38f994f7098504ffc1c5e
Alan Wang <alanwang611@gmail.com>
2023-04-28 21:06:38 -0400

    added tracker id

--- Alan/findTrackerId.py
+++ Alan/findTrackerId.py
@@ -1,38 +1,27 @@
-import csv
-from Levenshtein import distance
+import pandas as pd
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
-def find_track_id(artist, song, dataset):
-    min_distance = float('inf')
-    track_id = None
-    similar_artist = ""
-    similar_song = ""
+def find_track_id(artist, song, data):
+    artist_matches = process.extract(artist, data['artists'], limit=10, scorer=fuzz.token_sort_ratio)
+    best_artist_match = artist_matches[0]
 
-    with open(dataset, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        
-        for row in reader:
-            current_track_id, current_artist, current_song = row
-            if current_artist == artist and current_song == song:
-                return current_track_id
-            else:
-                current_distance = distance(song.lower(), current_song.lower())
-                if current_distance < min_distance:
-                    min_distance = current_distance
-                    track_id = current_track_id
-                    similar_artist = current_artist
-                    similar_song = current_song
+    if best_artist_match[1] >= 90:
+        artist_filtered_data = data[data['artists'] == best_artist_match[0]]
+    else:
+        artist_filtered_data = data
 
-    return track_id, similar_artist, similar_song
+    song_matches = process.extract(song, artist_filtered_data['track_name'], limit=1, scorer=fuzz.token_sort_ratio)
+    best_song_match = song_matches[0]
 
-artist = input("Enter artist name: ")
-song = input("Enter song name: ")
-dataset = "cleaned_data.csv"
+    track_id = artist_filtered_data.loc[artist_filtered_data['track_name'] == best_song_match[0]]['track_id'].values[0]
+    return track_id
 
-track_id, similar_artist, similar_song = find_track_id(artist, song, dataset)
+if __name__ == "__main__":
+    data = pd.read_csv('cleaned_data.csv')
 
-if track_id:
-    print(f"Found track id: {track_id}")
-    if similar_artist != artist or similar_song != song:
-        print(f"Most similar song: {similar_artist} - {similar_song}")
-else:
-    print("No track found.")
+    artist_name = input("Enter artist name: ")
+    song_name = input("Enter song name: ")
+
+    result_track_id = find_track_id(artist_name, song_name, data)
+    print("The track ID is:", result_track_id)
